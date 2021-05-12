d405d4f2b05608147d9feaeba1cc1901f0f8ee3f
de3key <gnosthard@gmail.com>
2021-05-12 14:19:06 +0700

    да

---
+++ Практическая работа/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задания номер 6.
+            Console.WriteLine("Введите число");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double v1 = a*b*l;
+            double v2 = l / (2 * Math.PI);
+            double r = b / 2;
+            double r1 = a / 2;         
+            double x = v1 - v2;
+            Console.WriteLine("%" + x);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Практическая работа/ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            double X1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            double Y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            double X2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            double Y2 = Convert.ToDouble(Console.ReadLine());             
+            double a = (Y1 - Y2) / (X1 - X2);
+            Console.WriteLine("a равно:" + a);
+            double b = (Y1 - a * X1);
+            Console.WriteLine("b равно:" + b);
+            Console.ReadKey();
+        }
+    }
+}
